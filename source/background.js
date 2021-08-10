c39b873c139d55e77b3d49bad92a6e78ea5df5f6
EnixCoda <enixcoda@gmail.com>
2021-08-10 22:16:39 +0800

    feat: support various google domains

--- source/background.js
+++ source/background.js
@@ -1,20 +1,28 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.contextMenus.create({
+import addDomainPermissionToggle from 'webext-domain-permission-toggle'
+import 'webext-dynamic-content-scripts'
+
+addDomainPermissionToggle({
+  title: 'Enable Feeling Lucky on this domain',
+  reloadOnSuccess: 'Refresh to activate?',
+})
+
+browser.runtime.onInstalled.addListener(function() {
+	browser.contextMenus.create({
 		title: `Feeling lucky for "%s"`,
 		contexts: ["selection"],
 		id: "feeling-lucky"
 	});
 });
 
-chrome.contextMenus.onClicked.addListener(function(info) {
+browser.contextMenus.onClicked.addListener(function(info) {
 	const selectionText = info.selectionText;
 	if (!selectionText) return;
 	const search = new URLSearchParams({
 		q: selectionText,
 		btnI: ""
 	});
-	chrome.tabs.create({
-		url: "http://www.google.com/search?" + search.toString(),
+	browser.tabs.create({
+		url: "https://www.google.com/search?" + search.toString(),
 		active: true
 	});
 });
--- source/content.js
+++ source/content.js
@@ -1,3 +1,12 @@
-const href = document.querySelector("a").href;
-document.body.innerHTML = `<a href="${href}">Redirecting...</a>`;
-location.href = href;
+const { hostname, pathname, searchParams } = new URL(window.location.href);
+
+// check href, like google.com/url?q=...
+if (
+	hostname.split(".").includes("google") &&
+	pathname.startsWith("/url") &&
+	searchParams.has("q")
+) {
+	const href = document.querySelector("a").href;
+	document.body.innerHTML = `<a href="${href}">Redirecting...</a>`;
+	location.href = href;
+}
